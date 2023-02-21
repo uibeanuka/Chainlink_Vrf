f4ecca7bfd1e6b2c929706ebd1a329c7814f2b5d
uibeanuka <uibeanuka@gmail.com>
2023-02-21 17:30:10 +0100

    my first commit

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,13 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
-}
+  const [owner, acct1,acct2,acct3] = await ethers.getSigners()
+  const randomgenerator=await ethers.getContractFactory("VRFv2Consumer")
+  const randomdeploy=await randomgenerator.deploy("10151")
+  await randomdeploy.deployed()
+  const contractaddress=randomdeploy.address
+  console.log(`Contract has been deployed to ${contractaddress}`)
+ }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
